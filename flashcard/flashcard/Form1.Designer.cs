54672eff8ec732db2e58edd86757cecb42bc0202
Pino Dat <hiendatcao13@gmail.com>
2023-10-20 07:36:51 +0700

    upload file code C#

---
+++ flashcard/flashcard/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace flashcard
+{
+    partial class frm_main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menu = new System.Windows.Forms.ToolStrip();
+            this.SuspendLayout();
+            // 
+            // menu
+            // 
+            this.menu.AutoSize = false;
+            this.menu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.menu.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(74, 640);
+            this.menu.TabIndex = 0;
+            this.menu.Text = "toolStrip1";
+            // 
+            // frm_main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(949, 640);
+            this.Controls.Add(this.menu);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MinimumSize = new System.Drawing.Size(965, 679);
+            this.Name = "frm_main";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip menu;
+    }
+}
+
---
+++ flashcard/flashcard/taskbar.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace flashcard
+{
+    public partial class frm_main : Form
+    {
+        public void TaskBar()
+        {
+            icon_logo();
+            icon_home();
+            icon_library();
+            icon_add();
+        }
+        private void icon_logo() //tạo logo và có bo cong tròn
+        {
+            PictureBox logo = new PictureBox();
+            logo.Image = global::flashcard.Properties.Resources.logo;
+            logo.InitialImage = null;
+            logo.Location = new System.Drawing.Point(3, 10);
+            logo.Name = "logo";
+            logo.Size = new System.Drawing.Size(96, 96);
+            logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            logo.TabIndex = 1;
+            logo.TabStop = false;
+            System.Drawing.Drawing2D.GraphicsPath gp = new System.Drawing.Drawing2D.GraphicsPath();
+            gp.AddEllipse(0, 0, logo.Width - 3, logo.Height - 3);
+            Region rg = new Region(gp);
+            logo.Region = rg;
+            Controls.Add(logo);
+            logo.BringToFront();
+        }
+        private void icon_home()
+        {
+            ToolStripButton iconHome = new System.Windows.Forms.ToolStripButton();
+            iconHome.AutoSize = false;
+            menu.ImageScalingSize = new Size(96, 96);
+            iconHome.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            iconHome.Image = global::flashcard.Properties.Resources.home;
+            iconHome.ImageTransparentColor = System.Drawing.Color.Magenta;
+            iconHome.Margin = new System.Windows.Forms.Padding(0, 120, 0, 0);
+            iconHome.Name = "iconHome";
+            iconHome.Size = new System.Drawing.Size(96, 96);
+            iconHome.Text = "Home";
+            iconHome.TextDirection = System.Windows.Forms.ToolStripTextDirection.Vertical90;
+            iconHome.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            iconHome.ImageScaling = ToolStripItemImageScaling.None;
+            menu.Items.Add(iconHome);
+        }
+        private void icon_library()
+        {
+            ToolStripButton iconLibrary = new System.Windows.Forms.ToolStripButton();
+            iconLibrary.AutoSize = false;
+            menu.ImageScalingSize = new Size(75, 75);
+            iconLibrary.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            iconLibrary.Image = global::flashcard.Properties.Resources.library;
+            iconLibrary.ImageTransparentColor = System.Drawing.Color.Magenta;
+            iconLibrary.Name = "iconLibrary";
+            iconLibrary.Size = new System.Drawing.Size(96, 96);
+            iconLibrary.Text = "Library";
+            iconLibrary.TextDirection = System.Windows.Forms.ToolStripTextDirection.Vertical90;
+            iconLibrary.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            iconLibrary.ImageScaling = ToolStripItemImageScaling.SizeToFit;
+            menu.Items.Add(iconLibrary);
+        }
+        private void icon_add()
+        {
+            ToolStripButton iconAdd = new System.Windows.Forms.ToolStripButton();
+            iconAdd.AutoSize = false;
+            menu.ImageScalingSize = new Size(75, 75);
+            iconAdd.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            iconAdd.Image = global::flashcard.Properties.Resources.add;
+            iconAdd.ImageTransparentColor = System.Drawing.Color.Magenta;
+            iconAdd.Name = "iconAdd";
+            iconAdd.Size = new System.Drawing.Size(96, 96);
+            iconAdd.Text = "Add";
+            iconAdd.TextDirection = System.Windows.Forms.ToolStripTextDirection.Vertical90;
+            iconAdd.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            iconAdd.ImageScaling = ToolStripItemImageScaling.SizeToFit;
+            menu.Items.Add(iconAdd);
+        }
+    }
+}
