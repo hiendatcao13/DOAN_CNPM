18ef2b79c9e3db5633cc264b0c52345409caaa1d
Linh <duylinh05030723@gmail.com>
2023-10-23 21:03:49 +0700

    Complete UI Sign_in and Register

--- flashcard/flashcard/frm_Sign_in.Designer.cs
+++ flashcard/flashcard/frm_Sign_in.Designer.cs
@@ -34,6 +34,9 @@
             this.txt_Password = new CustomControls.RJControls.RJTextBox();
             this.btn_Sign_in = new CustomControls.RJControls.RJButton();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbl_Register = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -42,7 +45,7 @@
             this.label1.Font = new System.Drawing.Font("Consolas", 30F);
             this.label1.Location = new System.Drawing.Point(449, 164);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(122, 34);
+            this.label1.Size = new System.Drawing.Size(130, 47);
             this.label1.TabIndex = 2;
             this.label1.Text = "Login";
             // 
@@ -50,49 +53,38 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(465, 217);
+            this.label2.Location = new System.Drawing.Point(459, 222);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(121, 22);
+            this.label2.Size = new System.Drawing.Size(149, 32);
             this.label2.TabIndex = 3;
             this.label2.Text = "User Name";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(457, 323);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(114,22);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Password";
             // 
             // txt_UserName
             // 
             this.txt_UserName.BackColor = System.Drawing.SystemColors.Window;
-            this.txt_UserName.BorderColor = System.Drawing.Color.MediumSlateBlue;
-            this.txt_UserName.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.txt_UserName.BorderColor = System.Drawing.Color.Black;
+            this.txt_UserName.BorderFocusColor = System.Drawing.Color.Black;
             this.txt_UserName.BorderRadius = 15;
             this.txt_UserName.BorderSize = 2;
             this.txt_UserName.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txt_UserName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txt_UserName.Location = new System.Drawing.Point(465, 258);
             this.txt_UserName.Margin = new System.Windows.Forms.Padding(4);
-            this.txt_UserName.Multiline = false;
+            this.txt_UserName.Multiline = true;
             this.txt_UserName.Name = "txt_UserName";
             this.txt_UserName.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
             this.txt_UserName.PasswordChar = false;
             this.txt_UserName.PlaceholderColor = System.Drawing.Color.DarkGray;
             this.txt_UserName.PlaceholderText = "";
-            this.txt_UserName.Location = new System.Drawing.Point(465, 258);
-            this.txt_UserName.Size = new System.Drawing.Size(549, 42);
+            this.txt_UserName.Size = new System.Drawing.Size(549, 43);
             this.txt_UserName.TabIndex = 4;
             this.txt_UserName.Texts = "";
             this.txt_UserName.UnderlinedStyle = false;
-            this.txt_UserName.Multiline = true;
             // 
             // txt_Password
             // 
             this.txt_Password.BackColor = System.Drawing.SystemColors.Window;
-            this.txt_Password.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.txt_Password.BorderColor = System.Drawing.Color.Black;
             this.txt_Password.BorderFocusColor = System.Drawing.Color.HotPink;
             this.txt_Password.BorderRadius = 15;
             this.txt_Password.BorderSize = 2;
@@ -100,17 +92,16 @@
             this.txt_Password.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.txt_Password.Location = new System.Drawing.Point(465, 364);
             this.txt_Password.Margin = new System.Windows.Forms.Padding(4);
-            this.txt_Password.Multiline = false;
+            this.txt_Password.Multiline = true;
             this.txt_Password.Name = "txt_Password";
             this.txt_Password.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
             this.txt_Password.PasswordChar = false;
             this.txt_Password.PlaceholderColor = System.Drawing.Color.DarkGray;
             this.txt_Password.PlaceholderText = "";
-            this.txt_Password.Size = new System.Drawing.Size(549, 42);
+            this.txt_Password.Size = new System.Drawing.Size(549, 43);
             this.txt_Password.TabIndex = 5;
             this.txt_Password.Texts = "";
             this.txt_Password.UnderlinedStyle = false;
-            this.txt_Password.Multiline = true;
             // 
             // btn_Sign_in
             // 
@@ -121,6 +112,7 @@
             this.btn_Sign_in.BorderSize = 0;
             this.btn_Sign_in.FlatAppearance.BorderSize = 0;
             this.btn_Sign_in.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Sign_in.Font = new System.Drawing.Font("Consolas", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_Sign_in.ForeColor = System.Drawing.Color.White;
             this.btn_Sign_in.Location = new System.Drawing.Point(826, 455);
             this.btn_Sign_in.Name = "btn_Sign_in";
@@ -129,13 +121,54 @@
             this.btn_Sign_in.Text = "Sign in";
             this.btn_Sign_in.TextColor = System.Drawing.Color.White;
             this.btn_Sign_in.UseVisualStyleBackColor = false;
-            this.btn_Sign_in.Font = new System.Drawing.Font("Consolas", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(457, 323);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 32);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(0, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(477, 522);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(294, 31);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Don\'t have an account yet? ";
+            // 
+            // lbl_Register
+            // 
+            this.lbl_Register.AutoSize = true;
+            this.lbl_Register.Font = new System.Drawing.Font("Segoe UI", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Register.Location = new System.Drawing.Point(766, 522);
+            this.lbl_Register.Name = "lbl_Register";
+            this.lbl_Register.Size = new System.Drawing.Size(97, 32);
+            this.lbl_Register.TabIndex = 10;
+            this.lbl_Register.Text = "Register";
             // 
             // frm_Sign_in
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1257, 714);
+            this.Controls.Add(this.lbl_Register);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.btn_Sign_in);
             this.Controls.Add(this.txt_Password);
@@ -157,5 +190,8 @@
         private CustomControls.RJControls.RJTextBox txt_Password;
         private CustomControls.RJControls.RJButton btn_Sign_in;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbl_Register;
     }
 }
